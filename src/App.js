8ab3394a0939c862377bb6701f2d1237b3b5d77f
jihadul-islam <jihadulislam708@gmail.com>
2020-04-18 22:08:13 -0700

    compilited

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AllUser from '../src/Component/AllUser/AllUser';
+import firebaseConfig from '../src/firebaseConfig';
+
+import '../src/'
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
+
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+import "firebase/analytics";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
+firebase.initializeApp(firebaseConfig);
+
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+    <div>
+  
+    <div className= "App"> <h1>Facebook</h1></div>
+    
+   
+     
+    
+    
+    <AllUser></AllUser>
+    
     </div>
+    
   );
+  
 }
 
 export default App;
